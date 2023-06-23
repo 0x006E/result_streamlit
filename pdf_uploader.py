ee84b42364bbb57ce96509236118ed647ea9672b
Nithin S Varrier <nithin486@hotmail.com>
2023-06-23 19:37:06 +0530

    add: docx support

--- pdf_uploader.py
+++ pdf_uploader.py
@@ -9,9 +9,15 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 from pdf_parser import parse_pdf
+from docxtpl import DocxTemplate
 import re
-
+import numpy as np
+from transpose_dict import TD
 st.set_page_config(layout="centered")
+
+
+def invert_dict(d):
+    return {value: key for key, value in d.items()}
 
 
 class log_viewer(logging.Handler):
@@ -19,19 +25,20 @@ class log_viewer(logging.Handler):
 
     # have a class member to store the existing logger
     logger_instance = logging.getLogger("camelot")
-    logger = logging.getLogger("pdf_parser")
+    logger = logging.getLogger()
     st_instance = st.empty()
 
     def __init__(self, st_instance=None, *args, **kwargs):
         # Initialize the Handler
         logging.Handler.__init__(self, *args)
         st_instance = st_instance or st.empty()
-
+        self.logger_instance.handlers.clear()
         self.logger_instance.addHandler(self)
         self.logger.addHandler(self)
 
     def emit(self, record):
         """ Overload of logging.Handler method """
+        record = self.format(record)
         self.st_instance.write(record)
 
 
@@ -70,9 +77,10 @@ def page_display():
     reader = PdfReader(st.session_state.uploaded_file)
     num_pages = len(reader.pages)
     st.write(f"The PDF contains {num_pages} pages.")
-    st.session_state.start_page = st.number_input(
+    col1, col2 = st.columns(2)
+    st.session_state.start_page = col1.number_input(
         "Start page", min_value=1, max_value=num_pages, step=1)
-    st.session_state.end_page = st.number_input(
+    st.session_state.end_page = col2.number_input(
         "End page", min_value=1, value=num_pages,  max_value=num_pages, step=1)
 
     # Convert the PDF to bytes
@@ -174,7 +182,6 @@ def convert_df(df):
 def page_display_table():
 
     st.header("Result")
-
     result = st.session_state.result
     branches = st.session_state.branches
     st.session_state.gpi = {
@@ -233,55 +240,67 @@ def page_display_table():
         with tab:
             branch_tabs = st.tabs(branches)
             for branch_tab, branch in zip(branch_tabs, branches):
-                try:
-                    with branch_tab:
-                        transformed_data = transform_data(result, branch, year)
-                        df = pd.DataFrame(
-                            data=transformed_data).set_index("Register Number")
-                        input = st.text_input(
-                            label="Filter criteria", key=year+branch, help="Eg: 30,62..118 includes 30, the numbers from 62 to 118 both inclusive", placeholder="Eg: 30,62..118")
-                        try:
-                            filter = generate_dataframe_filter(
-                                input, field_name="register_info_serial")
-                            if filter is None:
-                                filtered_df = df
-                            else:
-                                filtered_df = df.query(
-                                    filter)
-                        except DataFrameFilterException as e:
-                            st.error(str(e))
-                            filtered_df = df
-                        if year == f"'{latest_year}'":
-                            pie_df = filtered_df.groupby('Full pass').nunique()
-                            length = len(pie_df.index)
-                            if length > 1:
-                                values = ['Failed', 'Passed']
-                            else:
-                                if df["Full pass"][0]:
-                                    values = ["Passed"]
+                def function():
+                    try:
+                        with branch_tab:
+                            transformed_data = transform_data(
+                                result, branch, year)
+                            df = pd.DataFrame(
+                                data=transformed_data).set_index("Register Number")
+                            input = st.text_input(
+                                label="Filter criteria", key=year+branch, help="Eg: 30,62..118 includes 30, the numbers from 62 to 118 both inclusive", placeholder="Eg: 30,62..118")
+                            try:
+                                filter = generate_dataframe_filter(
+                                    input, field_name="register_info_serial")
+                                if filter is None:
+                                    filtered_df = df
                                 else:
-                                    values = ['Failed']
-                            pie_df.insert(0, 'Status', values)
-                            fig = px.pie(
-                                pie_df, values="register_info_serial", names="Status", title="Full pass percentage")
-                            st.plotly_chart(fig, use_container_width=True)
-                        filtered_df = filtered_df.sort_values('register_info_serial').drop(
-                            ["register_info_serial"], axis=1).reset_index()
-                        st.download_button(
-                            "Export CSV",
-                            convert_df(filtered_df),
-                            "file.csv",
-                            "text/csv",
-                            use_container_width=True,
-                            key='download-csv'+year+branch
-                        )
-                        st.dataframe(
-                            filtered_df,
-                            use_container_width=True,
-                        )
+                                    filtered_df = df.query(
+                                        filter)
+                            except DataFrameFilterException as e:
+                                st.error(str(e))
+                                filtered_df = df
+                            if year == f"'{latest_year}'":
+                                pie_df = filtered_df.groupby(
+                                    'Full pass').nunique()
+                                length = len(pie_df.index)
+                                if length > 1:
+                                    values = ['Failed', 'Passed']
+                                else:
+                                    if df["Full pass"][0]:
+                                        values = ["Passed"]
+                                    else:
+                                        values = ['Failed']
+                                pie_df.insert(0, 'Status', values)
+                                fig = px.pie(
+                                    pie_df, values="register_info_serial", names="Status", title="Full pass percentage")
+                                st.plotly_chart(fig, use_container_width=True)
+                            filtered_df = filtered_df.sort_values('register_info_serial').drop(
+                                ["register_info_serial"], axis=1).reset_index()
+                            col1, col2 = st.columns(2)
+                            col1.download_button(
+                                "Export CSV",
+                                convert_df(filtered_df),
+                                "file.csv",
+                                "text/csv",
+                                use_container_width=True,
+                                key='download-csv'+year+branch
+                            )
 
-                except KeyError:
-                    pass
+                            def generate_docx():
+                                st.session_state.filtered_df = filtered_df
+                                print(df)
+                                go_next()
+                            col2.button("Generate report", on_click=generate_docx,
+                                        key=f"generate-report-{branch}-{year}", use_container_width=True)
+                            st.dataframe(
+                                filtered_df,
+                                use_container_width=True,
+                            )
+
+                    except KeyError:
+                        pass
+                function()
     st.divider()
     st.caption("The following are the subjects and their credit points")
     st.table(
@@ -295,6 +314,108 @@ def page_display_table():
               use_container_width=True, on_click=go_back)
 
 
+def template_generation(context):
+    doc = DocxTemplate("template.docx")
+    doc.render(context)
+    doc.save("generated_doc.docx")
+
+
+def report_generation():
+    def go_back():
+        st.session_state.page_index -= 1
+    st.title("Report Generation")
+    if "filtered_df" not in st.session_state:
+        st.error("No view selected")
+        st.button("Go back", on_click=go_back)
+        st.stop()
+    df: pd.DataFrame = st.session_state.filtered_df
+    result = st.session_state.result
+    col1, col2 = st.columns(2)
+    dept = col1.text_input(label="Department name", )
+    year = col2.text_input(label="Year of admission")
+    staffadvisor1 = col1.text_input(label="Staff Advisor 1")
+    staffadvisor2 = col2.text_input(label="Staff Advisor 2")
+    semester = col1.text_input(label="Semester")
+    date = col2.date_input(label="Date of publication of result")
+    st.divider()
+    col1, col2 = st.columns(2)
+    witheld = col1.number_input(
+        label="No. of students whose results are withheld")
+    appearedinall = col2.number_input(
+        label="No. of students appeared in all subjects")
+    num_result_published = col1.number_input(
+        label="No. of students whose results published")
+    fullpass = col2.number_input(
+        label="No. of students passed in all subjects")
+    fullpass_percentage = st.number_input(label="Pass percentage")
+    st.divider()
+    staff_details = st.file_uploader(
+        "Staff details", type="csv", key="upload")
+    if staff_details is None:
+        st.error("Upload staff details")
+    else:
+        staff_details_df = pd.read_csv(staff_details)
+        col1, col2 = st.columns(2)
+        subject_code = col1.selectbox(
+            "Subject Code", options=staff_details_df.columns)
+        name_col = col2.selectbox(
+            "Staff Name", options=staff_details_df.drop(subject_code, axis=1).columns)
+        col1, col2 = st.columns(2)
+        subjects = df.columns.values[np.invert(np.in1d(df.columns.values, [
+            "Register Number", "Full pass", "SGPA", "register_info_serial"]))]
+        staff_details_df.set_index(subject_code, inplace=True)
+        st.write(staff_details_df)
+        subject_arr = []
+        passed_grades = ['S', "A+", "A", "B+", "B", "C", "C+", "D", "P"]
+        for index, subject in enumerate(subjects):
+            pass_with_fe = df[subject].value_counts(
+            )[passed_grades.extend(["FE", "Absent"])].sum()
+            pass_without_fe = df[subject].value_counts()[passed_grades].sum()
+            sub = {
+                'id': index,
+                'code': f"{subject} - {result['subjects'][subject]}",
+                'faculty': staff_details_df.loc[subject][name_col],
+                'num_reg': len(df.index),
+                'num_result': df[subject].count(),
+                'num_passed': df[subject].value_counts()[1],
+                'num_passwithfe': 0,
+                'num_passwithoutfe': 0
+            }
+            subject_arr.append(sub)
+        column_headers_dict = {
+            'id': 'Sl No.',
+            'code': 'Subject with Code',
+            'faculty': 'Name of Faculty',
+            'num_reg': 'No. of Students Registered',
+            'num_result': 'No. of Students whose results published',
+            'num_passed': 'No. of Students Passed',
+            'num_passwithfe': 'Pass % With FE and Absentees',
+            'num_passwithoutfe': 'Pass % without FE and Absentees',
+        }
+        editable_df = pd.DataFrame(subject_arr).set_index("id")
+        renamed_df = editable_df.rename(index=str, columns=column_headers_dict)
+        editable_df = st.data_editor(renamed_df).rename(
+            index=str, columns=invert_dict(column_headers_dict))
+        print(editable_df.to_dict(orient="records"))
+        context = {
+            'dept': dept,
+            'year': year,
+            'staffadvisor1': staffadvisor1,
+            'staffadvisor2': staffadvisor2,
+            'withheld': witheld,
+            'appearedinall': appearedinall,
+            'semester': semester,
+            'date': date,
+            'num_result_published': num_result_published,
+            'fullpass': fullpass,
+            'fullpass_percentage': fullpass_percentage,
+            'subjects': subject_arr,
+        }
+    col1.button("Generate", on_click=lambda: template_generation(context),
+                disabled=staff_details is None, use_container_width=True)
+    col2.button("Go back", on_click=go_back, use_container_width=True)
+
+
 # Initialize app state
 if "page_index" not in st.session_state:
     st.session_state.page_index = 0
@@ -305,7 +426,8 @@ pages = {
     1: page_display,
     2: process_page,
     3: page_result,
-    4: page_display_table
+    4: page_display_table,
+    5: report_generation
 }
 
 # Sidebar navigation
